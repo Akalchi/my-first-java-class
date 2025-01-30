5408fba2bf0409aba5f9e77b4907eb614230d61c
Akalchi <alejandrafdez1699@gmail.com>
2025-01-30 13:43:57 +0100

    "feat [Year of Birth is a number] - adding int, constructor, getter"

--- src/main/java/dev/alejandra/Person.java
+++ src/main/java/dev/alejandra/Person.java
@@ -4,19 +4,24 @@ public class Person {
 
     private String name;
     private String Id;
+    private int yearOfBirth;    
     
     // Constructor
-    public Person(String name, String Id) {
+    public Person(String name, String Id, int yearOfBirth) {
         this.name = name;
         this.Id = Id;
+        this.yearOfBirth = yearOfBirth;
     }
 
-    
+
     // Getters
     public String getName() {
         return this.name;
     }
     public String getId() {
         return this.Id;
+    }
+    public int getYearOfBirth() {
+        return this.yearOfBirth;
     }
 }
--- src/test/java/dev/alejandra/ClassTest.java
+++ src/test/java/dev/alejandra/ClassTest.java
@@ -10,7 +10,7 @@ public class ClassTest {
     @Test
     @DisplayName("Person class test")
     void person_test() {
-        Person person = new Person("Alejandra", "12345678L");
+        Person person = new Person("Alejandra", "12345678L",1999);
         assertThat(person, is (instanceOf(Person.class)));
         
     }
@@ -18,22 +18,30 @@ public class ClassTest {
     @Test
     @DisplayName("Person name is a string")
     void person_name_test() {
-        Person person = new Person("perro", "12345678L");
+        Person person = new Person("perro", "12345678L", 1999);
         assertThat(person.getName(), is (instanceOf(String.class)));
     }
     
     @Test
     @DisplayName("Person ID is a string")
     void person_ID_test() {
-        Person person = new Person("gato", "12345678L");
+        Person person = new Person("gato", "12345678L", 1999);
         assertThat(person.getId(), is (instanceOf(String.class)));
     }
 
     @Test
     @DisplayName("ID has 9 characters")
     void person_ID_lenght_test() {
-        Person person = new Person("gato", "12345678L");
+        Person person = new Person("gato", "12345678L", 1999);
         assertThat(person.getId().length(), is(9));
+    }
+
+    @Test
+    @DisplayName("Year of birth is a number")
+    void person_year_of_birth_test() {
+        Person person = new Person("gato", "12345678L" , 1999);
+        assertThat(person.getYearOfBirth(), is (instanceOf(Integer.class)));
+        
     }
 
 }
